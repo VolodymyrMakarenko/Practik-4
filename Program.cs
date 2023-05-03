7fc048a4d0a05792840dea9d8070f6b3259bd2da
VolodymyrMakarenko <118749504+VolodymyrMakarenko@users.noreply.github.com>
2023-05-03 20:06:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        string word = "Predator";
+        int uniqueLetters = CountUniqueLetters(word);
+        Console.WriteLine("У слові \"" + word + "\" є " + uniqueLetters + " різних літер.");
+    }
+
+    static int CountUniqueLetters(string word)
+    {
+        int count = 0;
+        bool[] seen = new bool[26];  
+
+        
+        foreach (char c in word.ToLower())
+        {
+            
+
+            int index = c - 'a';  
+            if (index >= 0 && index < 26 && !seen[index])
+            {
+                
+                seen[index] = true;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
